a606e96ab1eb787e1702c1b4ecb52010532ff964
prashantinim <prashantinimaniam11@gmail.com>
2023-11-24 00:21:12 +1100

    Initial Commit

---
+++ Rewild/RewildApp.swift
@@ -0,0 +1,17 @@
+//
+//  RewildApp.swift
+//  Rewild
+//
+//  Created by Prashantini Maniam on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RewildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
